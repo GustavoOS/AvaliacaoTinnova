7fb827bf0ef1f22fa09dec8b08425ff5a69ef9b7
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2021-03-20 13:23:24 -0300

    Improve test description @ activity 2

--- atividade2/test/sort.spec.js
+++ atividade2/test/sort.spec.js
@@ -2,10 +2,10 @@ const bubbleSort = require("../src");
 
 /*
     GIVEN [5, 3, 2, 4, 7, 1, 0, 6] as an array
-    WHEN calling bubble sort method
+    WHEN calling bubble sort function
     THEN every array element should equal its index
 */
-test('Test Bubble Sort', () => {
+test('Test Bubble Sort with Random Order', () => {
     const arr = [5, 3, 2, 4, 7, 1, 0, 6];
     bubbleSort(arr);
     assertElementEqualIndex(arr); //arr[0] = 0 and so on
@@ -13,10 +13,10 @@ test('Test Bubble Sort', () => {
 
 /*
     GIVEN [0, 1, 2, 3, 4, 5, 6, 7] as an array
-    WHEN calling bubble sort method
+    WHEN calling bubble sort function
     THEN every array element should equal its index
 */
-test('Test already sorted', () => {
+test('Test sort with array already sorted as input', () => {
     const arr = [0, 1, 2, 3, 4, 5, 6, 7];
     bubbleSort(arr);
     assertElementEqualIndex(arr);
@@ -24,10 +24,10 @@ test('Test already sorted', () => {
 
 /*
     GIVEN [7, 6, 5, 4, 3, 2, 1, 0] as an array
-    WHEN calling bubble sort method
+    WHEN calling bubble sort function
     THEN every array element should equal its index
 */
-test('Test reverse sorting', () =>{
+test('Test sort with a reverse sorted array', () =>{
     const arr = [7, 6, 5, 4, 3, 2, 1, 0];
     bubbleSort(arr);
     assertElementEqualIndex(arr);
